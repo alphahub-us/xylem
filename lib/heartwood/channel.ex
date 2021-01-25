66696d21537f1c0751445fd99a0c6776a554ac9a
Derek Meer <derek@meer.email>
2021-01-25 11:30:56 -0800

    add Heartwood.Channel for pub/sub capabilities
    We'll need publish/subscribe capabilities since multiple bots may
    want to use a single source of signals. Thus, this commit adds the
    Heartwood.Channel module as a wrapper around Phoenix.PubSub (added to
    deps).

---
+++ lib/heartwood/channel.ex
@@ -0,0 +1,30 @@
+defmodule Heartwood.Channel do
+  @moduledoc """
+  Wrapper around Phoenix.PubSub. This allows various heartwood processes to
+  broadcast/subscribe to each other.
+  """
+
+  alias Phoenix.PubSub
+  @me __MODULE__
+
+  @doc false
+  def child_spec(_), do: PubSub.child_spec(name: @me)
+
+  @doc """
+  Subscribe to a topic.
+  """
+  @spec subscribe(binary) :: :ok | {:error, term}
+  def subscribe(topic), do: PubSub.subscribe(@me, topic)
+
+  @doc """
+  Unsubscribe from a topic.
+  """
+  @spec unsubscribe(binary) :: :ok | {:error, term}
+  def unsubscribe(topic), do: PubSub.unsubscribe(@me, topic)
+
+  @doc """
+  Broadcast a message to a topic's subscribers.
+  """
+  @spec broadcast(binary, term) :: :ok | {:error, term}
+  def broadcast(topic, message), do: PubSub.broadcast(@me, topic, message)
+end
