ee1ac3c1b08fbb0ab4db7f1b01897f69d3334489
Derek Meer <derek@meer.email>
2021-01-26 20:25:13 -0800

    add Heartwood.Registry
    Some venues/markets/sources will need to be named for ease of access by
    bots who subscribe to them. These publishers might also run as processes
    themselves. That's why we bring in a simple wrapper around the Registry
    module to name processes.
    
    We associate the name of the module with the publisher name as well,
    making it easy to call the topics() functions.

---
+++ lib/heartwood/registry.ex
@@ -0,0 +1,27 @@
+defmodule Heartwood.Registry do
+  @moduledoc """
+  Registry for Heartwood processes. It's a simple wrapper around Registry.
+  """
+
+  @me __MODULE__
+
+  def child_spec(_), do: Registry.child_spec(keys: :unique, name: @me)
+
+  @doc """
+  Register a process via the Registry module.
+  """
+  @spec register(atom, module) :: {:ok, pid} | {:error, {:already_registered, pid}}
+  def register(name, module), do: Registry.register(@me, name, module)
+
+  @doc """
+  Find the process with the given name via the Registry.
+  """
+  @spec lookup(atom) :: {pid, module} | nil
+  def lookup(name) do
+    Registry.lookup(@me, name)
+    |> case do
+      [match] -> match
+      _ -> nil
+    end
+  end
+end
