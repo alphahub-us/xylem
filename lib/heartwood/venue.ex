de0591568b9fe1cf1a51db56d141dd0aeb8895ce
Derek Meer <derek@meer.email>
2021-02-01 11:12:59 -0800

    add submit_order, cancel_order, and get_positions callbacks
    Bots need to use venues for order updates, but they also need to place
    and cancel orders. This commit adds those callbacks, along with a
    callback to get the currently held positions for a venue account.

--- lib/heartwood/venue.ex
+++ lib/heartwood/venue.ex
@@ -8,12 +8,13 @@ defmodule Heartwood.Venue do
   Venue order events. These are updates sent by the venue on a given order.
   """
   @type order_event :: %{
+    id: String.t,
     timestamp: NaiveDateTime.t,
-    type: :fill | :partial_fill | :new | :cancelled,
-    side: :buy | :sell,
+    type: :fill | :partial | :new | :cancel,
     symbol: String.t,
-    qty: integer,
-    price: float
+    side: :buy | :sell,
+    qty: non_neg_integer,
+    price: float,
   }
 
   @doc """
@@ -21,7 +22,26 @@ defmodule Heartwood.Venue do
   """
   @callback topic(options :: keyword) :: String.t | [String.t]
 
-  @optional_callbacks topic: 1
+  @doc """
+  Submits an order to the venue
+  """
+  @callback submit_order(order :: map, options :: keyword) :: :ok
+  @callback submit_order(venue :: pid, order :: map, options :: keyword) :: :ok
+
+  @doc """
+  Submits an order cancellation request to the venue
+  """
+  @callback cancel_order(order :: map, options :: keyword) :: :ok
+  @callback cancel_order(venue :: pid, order :: map, options :: keyword) :: :ok
+
+  @doc """
+  gets the positions from the venue
+  """
+  @callback get_positions() :: :ok
+  @callback get_positions(venue :: pid) :: :ok
+
+  @optional_callbacks topic: 1, submit_order: 2, submit_order: 3, cancel_order: 2,
+  cancel_order: 3, get_positions: 0, get_positions: 1
 
   @spec event_to_csv(order_event) :: String.t
   def event_to_csv(event) do
@@ -29,5 +49,32 @@ defmodule Heartwood.Venue do
     |> Enum.map(&Map.get(event, &1))
     |> Enum.reject(&is_nil/1)
     |> Enum.join(",")
+  end
+
+  def submit_order(venue_name, order, options \\ []) do
+    venue_name
+    |> Heartwood.Registry.lookup()
+    |> case do
+      {pid, module} -> apply(module, :submit_order, [pid, order, options])
+      _ -> apply(venue_name, :submit_order, [order, options])
+    end
+  end
+
+  def cancel_order(venue_name, order, options \\ []) do
+    venue_name
+    |> Heartwood.Registry.lookup()
+    |> case do
+      {pid, module} -> apply(module, :cancel_order, [pid, order, options])
+      _ -> apply(venue_name, :cancel_order, [order, options])
+    end
+  end
+
+  def get_positions(venue_name) do
+    venue_name
+    |> Heartwood.Registry.lookup()
+    |> case do
+      {pid, module} -> apply(module, :get_positions, [pid])
+      _ -> apply(venue_name, :get_positions, [])
+    end
   end
 end
