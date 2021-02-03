2f5ae37a9807414ba188f59c4b9395685e28133a
Derek Meer <derek@meer.email>
2021-02-02 21:19:49 -0800

    customize console logging messages

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,8 @@ config :tesla, adapter: Tesla.Adapter.Gun
 
 config :xylem, ledger: [data_dir: "/Users/derek/work/quantonomy/xylem"]
 
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n"
+
 import_config "#{Mix.env()}.exs"
