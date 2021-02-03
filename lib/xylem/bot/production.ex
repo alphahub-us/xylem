a506b24382b88ec7131f7ac4dac7f37cb6714a32
Derek Meer <derek@meer.email>
2021-02-02 21:41:00 -0800

    add name identifier to output messages

--- lib/xylem/bot/production.ex
+++ lib/xylem/bot/production.ex
@@ -11,7 +11,7 @@ defmodule Xylem.Bot.Production do
 
   @impl true
   def handle_info({:signal, signals}, state = %{venue: venue, name: name}) do
-    IO.inspect(signals, label: "signals")
+    IO.inspect(signals, label: "[#{name}] signals")
     positions = Venue.get_positions(venue)
     signals
     |> Ledger.prepare_orders(name, positions)
@@ -19,13 +19,13 @@ defmodule Xylem.Bot.Production do
     {:noreply, state}
   end
 
-  def handle_info({:data, data}, config) do
-    IO.inspect(data, label: "market data")
-    {:noreply, config}
+  def handle_info({:data, data}, state = %{name: name}) do
+    IO.inspect(data, label: "[#{name}] market data")
+    {:noreply, state}
   end
 
   def handle_info({:venue, update}, state = %{name: name}) do
-    IO.inspect(update, label: "venue update")
+    IO.inspect(update, label: "[#{name}] venue update")
     Xylem.Logger.record_order_event(name, update, &Venue.event_to_csv/1)
     Ledger.process_event(update)
     {:noreply, state}
