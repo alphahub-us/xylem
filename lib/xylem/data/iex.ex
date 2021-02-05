5f58d6dcdd3989d3c2642e25779beb6701fbdcbe
Derek Meer <derek@meer.email>
2021-02-05 13:11:00 -0800

    fix signature of IEx broadcast message

--- lib/xylem/data/iex.ex
+++ lib/xylem/data/iex.ex
@@ -6,5 +6,5 @@ defmodule Xylem.Data.IEx do
   @impl Xylem.Data
   def topic(_), do: {:ok, @topic}
 
-  def send_event(event), do: Xylem.Channel.broadcast(@topic, {:market, event})
+  def send_event(event), do: Xylem.Channel.broadcast(@topic, {:data, event})
 end
