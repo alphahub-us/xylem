da736733c67acf68feff306943866d2bc29fa4ff
Derek Meer <derek@meer.email>
2021-05-24 18:58:52 -0700

    remove my settings from default config

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,6 @@ import Config
 
 config :tesla, adapter: Tesla.Adapter.Gun
 
-config :xylem, ledger: [data_dir: "/Users/derek/work/quantonomy/xylem"]
-
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n"
